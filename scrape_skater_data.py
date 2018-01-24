065b600c8787c5bb38515e9ef8cfc127ba76ffe0
ninaanatchkova <rinsingwind@gmail.com>
2018-01-24 13:21:28 +0200

    trying to write to file

--- scrape_skater_data.py
+++ scrape_skater_data.py
@@ -1,4 +1,6 @@
-import requests
+import os
+import json
+import datetime
 from skaters import skaters
 from links import skater_total_segment_data
 
@@ -13,6 +15,40 @@ def get_segment_scores_for_all_skaters(segment):
         all_segment_scores.append(segment_scores)
     return all_segment_scores
 
+# create project folder
+def create_project_dir(directory):
+    if not os.path.exists(directory):
+        print('Creating folder ' + directory)
+        os.makedirs(directory)
 
+# create a new file
+def write_file(path, data):
+    f = open(path, 'w')
+    f.write(data)
+    f.close()
+
+# add data onto an existing file
+def append_to_file(path, data):
+    with open(path, 'a') as outfile:
+        json.dump(data, outfile)
+
+# delete the contents of a file
+def delete_file_contents(path):
+    with open(path, 'w'):
+        pass
+
+def convert_dates_to_string(o):
+    if isinstance(o, datetime.datetime):
+        return o.__str__()
+
+create_project_dir("data")
+
+write_file("data/short_program_scores.txt", "All short program scores from the 2016/2017 and 2017/2018 seasons for the top 10 men singles skaters participating at the 2018 Olympics \n")
 all_short_program_scores = get_segment_scores_for_all_skaters("short")
+append_to_file("data/short_program_scores.txt", all_short_program_scores)
+
+write_file("data/long_program_scores.txt", "All long program scores from the 2016/2017 and 2017/2018 seasons for the top 10 men singles skaters participating at the 2018 Olympics \n")
 all_long_program_scores = get_segment_scores_for_all_skaters("long")
+append_to_file("data/long_program_scores.txt", all_long_program_scores)
+
+
